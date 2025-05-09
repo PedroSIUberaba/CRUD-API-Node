c01575dfa3d0436d9a2b562cf450dffe13e9f21b
Pedro <pedhenriquecm@gmail.com>
2025-05-09 12:12:50 -0300

    Update: Optimizing API

--- src/app.js
+++ src/app.js
@@ -1,38 +1,8 @@
-// import { openDb } from './configDB.js';
-import { createTable, insertPessoa } from './Controler/Pessoa.js';
-
 import express from 'express';
-
 const app = express();
 app.use(express.json());
 
-createTable();
-
-app.get('/', function (req, res) { 
-    res.send('Hello World!');
-});
-
-app.post('/pessoas', function (req, res) {
-    insertPessoa(req.body);
-    res.json({
-        "statusCode": 200,
-        "message": 'Pessoa cadastrada com sucesso!'
-    });
-});
-
-app.put('/pessoas', function (req, res) {
-    if(req.body && !req.body.id) {
-        res.json({
-            "statusCode": 400,
-            "message": 'ID não informado!'
-        });
-    }else{
-        updatePessoa(req.body);
-        res.json({
-            "statusCode": 200,
-            "message": 'Pessoa cadastrada com sucesso!'
-        }); 
-    }
-});
+import router from './routes.js';
+app.use(router);
 
 app.listen(3000, ()=>console.log('API Rodando.'));
