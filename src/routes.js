84d5272a23ee0edb5c1c1b88b7cf3b687c25e836
Pedro <pedhenriquecm@gmail.com>
2025-05-09 12:13:12 -0300

    Update: Optimizing API

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { createTable, insertPessoa, selectPessoa, selectPessoas,updatePessoa, deletePessoa} from './Controler/Pessoa.js';
+
+const router = Router();
+
+router.get('/', (req, res) => {
+    res.json({
+        "statusCode": 200,
+        "message": 'API Rodando!'
+    })
+});
+
+router.get('/pessoas', selectPessoas)
+router.get('/pessoa', selectPessoa)
+router.post('/pessoa', insertPessoa)
+router.put('/pessoa', updatePessoa)
+router.delete('/pessoa', deletePessoa)
+
+export default router;
